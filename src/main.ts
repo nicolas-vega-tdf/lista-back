b91f8b1633042c1f4d0acae572fe1e9c7a48f02c
nicolas-vega-tdf <nicolasvega@tdf.edu.ar>
2023-11-09 21:07:57 -0300

    correciones

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:['https://lista-back-vrqx.onrender.com'],
+    origin:['http://localhost:3001','https://lista-tarea-83ccb.web.app/'],
     methods:'GET,PUT,POST,DELETE,PATH',
     credentials:true
   })
